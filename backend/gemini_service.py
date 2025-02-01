5c806b5be405dc90612c84a71fa2ea5e3c27f72f
Peter Galati <peter8galati@gmail.com>
2025-02-01 19:52:36 +0000

    new gemini

--- backend/gemini_service.py
+++ backend/gemini_service.py
@@ -4,12 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+print(os.getenv('GEMINI_API_KEY'))
+
 def configure_ai():
     api_key = os.getenv('GEMINI_API_KEY')  # Get the API key from the .env file
     if api_key is None:
         raise ValueError("API key not found in environment variables")
     genai.configure(api_key=api_key)
-    return genai.GenerativeModel("gemini-1.5-flash")
+    return genai.GenerativeModel("gemini-2.0-flash-exp")
 
 def generate_content(prompt: str):
     model = configure_ai()
